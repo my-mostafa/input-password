3bc48f706083a5bf3474cbc8d09e47afca10a6fb
mostafa <immostafaeh@gmail.com>
2024-10-30 00:45:11 +0330

    input password

---
+++ js/script.js
@@ -0,0 +1,30 @@
+﻿ var $ = document;
+ function poiid(poiname) {
+   return $.getElementById(poiname);
+ }
+
+ function poiass(spoiname) {
+   return $.getElementsByClassName(spoiname);
+   
+ }
+
+ var togglePassword = poiass("toggle-password");
+ var passwordField = poiid("password-field");
+
+ togglePassword[0].addEventListener("click", function () {
+   if (passwordField.type == "text") {
+     passwordField.type = "password";
+     togglePassword[0].classList.remove("active");
+
+    } else {
+     passwordField.type = "text";
+     togglePassword[0].classList.add("active");
+   }
+ })
+
+
+
+
+
+
+
